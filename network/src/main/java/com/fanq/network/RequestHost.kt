c6c00ba7951392d52bd38a56586da3fdc082c1ea
范启 <fanqi@inke.cn>
2023-10-07 11:33:28 +0800

    [feat]-remove

--- network/src/main/java/com/fanq/network/RequestHost.kt
+++
@@ -1,9 +0,0 @@
-package com.fanq.network
-
-import kotlin.reflect.KClass
-
-@Target(AnnotationTarget.ANNOTATION_CLASS, AnnotationTarget.CLASS)
-@Retention(AnnotationRetention.RUNTIME)
-annotation class RequestHost(
-    val host: String = ""
-)
